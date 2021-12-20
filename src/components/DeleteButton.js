8f86fc6b1b92f117c589db104f3b6d72e300a1c3
Emily Landers <emily.j.landers@gmail.com>
2021-12-20 17:15:58 -0500

    basic setup

---
+++ src/components/DeleteButton.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DeleteButton extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
