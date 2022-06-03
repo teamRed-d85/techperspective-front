d315affa5bfa15c9a1f06d6b536aabbae824b8df
Dana Huffman <dana.huffman@yahoo.com>
2022-06-02 17:16:23 -0700

    More netlify fixes

--- src/components/ActiveSurveyContainer.js
+++ src/components/ActiveSurveyContainer.js
@@ -12,13 +12,13 @@ class ActiveSurveyContainer extends Component {
 
                 <div style={{ justifyContent: "center" }} >
 
-                    {this.props.activeSurvey && this.props.activeSurvey.map((survey, idx) => {
+                    {this.props.activeSurvey && (this.props.activeSurvey.map((survey, idx) => {
                         return (
 
                             <ActiveSurvey graphResults={this.props.graphResults} openModal={this.props.openModal} key={idx} activeSurvey={survey} />
 
                         )
-                    })}
+                    }))}
 
                     {/*{this.props.activeSurvey && <ActiveSurvey graphResults={this.props.graphResults} activeSurvey={this.props.activeSurvey} openModal={this.props.openModal} getActiveSurvey={this.props.getActiveSurvey} />}*/}
                 </div>
